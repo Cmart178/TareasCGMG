b282a2be81caff1f84194e101796ff48b5865d51
Cmart178 <124319034+Cmart178@users.noreply.github.com>
2023-02-20 21:41:06 -0600

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -180,6 +180,29 @@ int main()
         } while (true);
         return 0;
     }
+    if (opus==10){
+    bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+    int n;
+    cout << "Enter an integer: ";
+    cin >> n;
+    if (isPrime(n)) {
+        cout << n << " is a prime number." << endl;
+    } else {
+        cout << n << " is not a prime number." << endl;
+    }
+    return 0;
+}
+    }
     if (opus > 10) {
         cout << "Opcion no valida" << std::endl;
     }
